f565074697178773d8697dc9de75f836283c201f
Samuconfaa <samuconfa08@gmail.com>
2026-04-21 21:24:34 +0200

    Implementa LoginPage e LoginViewModel con autenticazione reale

--- src/Turnify.Mobile/MauiProgram.cs
+++ src/Turnify.Mobile/MauiProgram.cs
@@ -1,4 +1,7 @@
+using System;
 using Microsoft.Extensions.Logging;
+using Turnify.Core.Interfaces.Services;
+using Turnify.Mobile.Services;
 using Turnify.Mobile.ViewModels;
 using Turnify.Mobile.Views;
 
@@ -6,20 +9,27 @@ namespace Turnify.Mobile;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
+
+        // Services
+        builder.Services.AddHttpClient<IAuthService, AuthService>(client =>
+        {
+            // Base URL dell'API (su emulatore Android 10.0.2.2 mappa a localhost)
+            client.BaseAddress = new Uri("http://10.0.2.2:5000"); 
+        });
 
         // ViewModels
         builder.Services.AddTransient<LoginViewModel>();
@@ -37,6 +47,6 @@ public static class MauiProgram
         builder.Services.AddTransient<NotificationsPage>();
         builder.Services.AddTransient<ProfilePage>();
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }

1e8d2a3376d8ae4bd3cf1dcef492dfaa6af6c88e
Samuconfaa <samuconfa08@gmail.com>
2026-05-13 20:36:08 +0200

    release: APK e AAB firmati v1.0 + aggiorna .gitignore e csproj

--- src/Turnify.Mobile/ViewModels/VacationListViewModel.cs
+++ src/Turnify.Mobile/ViewModels/VacationListViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
